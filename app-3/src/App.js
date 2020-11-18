8c9ce592a412f41f53fda5246554f5e015527860
Sergio Vega <hekinky@MacBook-Pro.local>
2020-11-18 16:00:49 -0700

    [Complete]

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component (){
+  constructor(){
+    super()
 
-export default App;
+    this.state = {
+      filterString: "",
+      rides: ["motorcycle", "car", "plane", "batmobile", "bike"]
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({filterString: filter})
+  }
+
+  render() {
+    let ridesToDisplay = this.state.rides.filter((element,index) => {return element.includes(this.state.filterString)})
+    .map((element, index) => {return <h2 key={index}>{element}</h2>})
+  
+    return(
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text"/>
+        {ridesToDisplay}
+      </div>
+    )
+  }
+} 
+
+  
+
